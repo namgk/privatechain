d772f0700c5257600c654548b729d2493eea66a1
Nam Giang <kyng@ece.ubc.ca>
2018-11-26 16:03:35 -0800

    change network name

--- truffle-config.js
+++ truffle-config.js
@@ -3,13 +3,13 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   // from: the account truffle is deploying from.
   networks: {
-    development: {
+    ganache: {
       host: "0.0.0.0",
       port: 7545,
       from: "0x9451988c41266A7faD710ab07153798f26035037",
       network_id: "*" // Match any network id
     },
-    mooveechain: {
+    gethprivate: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" 
